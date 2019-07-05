cb1d628e9129de2072c0f39196b28ae2853b433a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-07-05 18:58:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# adresse-data-legacy-api
+
+API permettant de télécharger les données Adresse "comme avant" :)
+
+## URL supportées
+
+- `/data/ban-v0/BAN_licence_gratuite_repartage_xxx.zip`
+
+## Utilisation
+
+```bash
+yarn --prod
+yarn start
+```
+
+## Licence
+
+MIT
---
+++ server.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+require('dotenv').config()
+
+const {join} = require('path')
+const {createReadStream} = require('fs')
+const {createGunzip} = require('zlib')
+const express = require('express')
+const cors = require('cors')
+const {ZipFile} = require('yazl')
+const pumpify = require('pumpify')
+const contentDisposition = require('content-disposition')
+const departements = require('@etalab/decoupage-administratif/data/departements.json').map(d => d.code)
+
+const app = express()
+
+const {ADRESSE_DATA_PATH} = process.env
+
+function getFileStream(departement) {
+  return pumpify(
+    createReadStream(join(ADRESSE_DATA_PATH, `adresses-${departement}.csv.gz`)),
+    createGunzip()
+  )
+}
+
+app.use(cors())
+
+app.get('/data/ban-v0/BAN_licence_gratuite_repartage.zip', (req, res) => {
+  const zip = new ZipFile()
+  departements.forEach(departement => {
+    zip.addReadStream(getFileStream(departement), `BAN_licence_gratuite_repartage_${departement}.csv`)
+  })
+  res.set('Content-Disposition', contentDisposition('BAN_licence_gratuite_repartage.zip'))
+  res.type('zip')
+  zip.outputStream.pipe(res)
+  zip.end()
+})
+
+app.get('/ban-v0/BAN_licence_gratuite_repartage_:departement.zip', (req, res) => {
+  const {departement} = req.params
+  const zip = new ZipFile()
+  zip.addReadStream(getFileStream(departement), `BAN_licence_gratuite_repartage_${departement}.csv`)
+  res.set('Content-Disposition', contentDisposition(`BAN_licence_gratuite_repartage_${departement}.zip`))
+  res.type('zip')
+  zip.outputStream.pipe(res)
+  zip.end()
+})
+
+app.listen(process.env.PORT || 5000)
